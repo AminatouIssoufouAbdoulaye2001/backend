8e92da18c0c6a2b4f53ab3db4afb5ca343e2ee6f
abdellah098 <saley.abdellah@gmail.com>
2022-04-02 13:27:27 +0200

    tech(compte admin): création d'un compte administrateur par défaut

--- src/main/java/Pfe/SpringBoot/BackEnd/BackEndApplication.java
+++ src/main/java/Pfe/SpringBoot/BackEnd/BackEndApplication.java
@@ -1,13 +1,38 @@
 package Pfe.SpringBoot.BackEnd;
 
+import Pfe.SpringBoot.BackEnd.constantes.ERole;
+import Pfe.SpringBoot.BackEnd.entities.User;
+import Pfe.SpringBoot.BackEnd.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collection;
+
 @SpringBootApplication
-public class BackEndApplication {
+public class BackEndApplication implements CommandLineRunner {
+	@Autowired
+	UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Collection<User> users = (Collection<User>)userRepository.findAll();
+		if (users.isEmpty()) {
+			// le compte de l'administrateur est le premier
+			User admin = new User();
+			admin.setFirstName("administrator");
+			admin.setLastName("root");
+			admin.setEmail("admin.nghost@tn.com");
+			admin.setPassword("adminù@!785nghost");
+			admin.setRole(ERole.ROLE_ADMIN);
+
+			userRepository.save(admin);
+		}
+
+	}
 }
