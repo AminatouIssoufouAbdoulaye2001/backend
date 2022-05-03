206516c4053d6ba4aec7f91cd961b346f1c08b5c
Oummyia <aminatouoummyia@gmail.com>
2022-05-03 22:35:59 +0100

    database

--- src/main/java/Pfe/SpringBoot/BackEnd/entities/DataBase.java
+++ src/main/java/Pfe/SpringBoot/BackEnd/entities/DataBase.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name = "databases")
+@Entity(name = "databasess")
 public class DataBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
